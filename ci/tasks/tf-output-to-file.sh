7306197a72c449308870336ad010ad3fb185ff13
Daniel Jones <daniel.jones@engineerbetter.com>
2019-05-17 10:20:15 +0100

    extract kubeconfig, but don't do anything with it yet

---
+++ ci/tasks/tf-output-to-file.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -euo pipefail
+
+: "${METADATA_KEY:?METADATA_KEY env var must be provide the key name to look up in metadata}"
+: "${OUTPUT_FILE:?OUTPUT_FILE env var must provide full name of file to write into tf-output-to-file output dir}"
+
+jq -r ".${METADATA_KEY}" terraform/metadata > "${OUTPUT_FILE}"
