8b87414b8faa6187a6f516900f9bdc9e5ce198e2
Daniel Jones <daniel.jones@engineerbetter.com>
2019-05-17 08:19:52 +0100

    generate kubeconfig in tf

--- ci/tf/gke/cluster.tf
+++ ci/tf/gke/cluster.tf
@@ -1,6 +1,19 @@
+resource "random_id" "username" {
+  byte_length = 14
+}
+
+resource "random_id" "password" {
+  byte_length = 16
+}
+
 resource "google_container_cluster" "scf-cluster" {
   name               = "scf-cluster"
   initial_node_count = 1
+
+  master_auth {
+    username = "${random_id.username.hex}"
+    password = "${random_id.password.hex}"
+  }
 
   node_config {
     image_type   = "UBUNTU"
@@ -8,15 +21,25 @@ resource "google_container_cluster" "scf-cluster" {
   }
 }
 
-# The following outputs allow authentication and connectivity to the GKE Cluster.
-output "client_certificate" {
-  value = "${google_container_cluster.primary.master_auth.0.client_certificate}"
+data "template_file" "kubeconfig" {
+  template = "${file("${path.module}/kubeconfig-template.yaml")}"
+
+  vars {
+    cluster_name    = "${google_container_cluster.scf-cluster.name}"
+    user_name       = "${google_container_cluster.scf-cluster.master_auth.0.username}"
+    user_password   = "${google_container_cluster.scf-cluster.master_auth.0.password}"
+    endpoint        = "${google_container_cluster.scf-cluster.endpoint}"
+    cluster_ca      = "${google_container_cluster.scf-cluster.master_auth.0.cluster_ca_certificate}"
+    client_cert     = "${google_container_cluster.scf-cluster.master_auth.0.client_certificate}"
+    client_cert_key = "${google_container_cluster.scf-cluster.master_auth.0.client_key}"
+  }
 }
 
-output "client_key" {
-  value = "${google_container_cluster.primary.master_auth.0.client_key}"
+resource "local_file" "kubeconfig" {
+  content  = "${data.template_file.kubeconfig.rendered}"
+  filename = "${path.module}/kubeconfig"
 }
 
-output "cluster_ca_certificate" {
-  value = "${google_container_cluster.primary.master_auth.0.cluster_ca_certificate}"
+output "kubeconfig_content" {
+  value = "${local_file.kubeconfig.content}"
 }
