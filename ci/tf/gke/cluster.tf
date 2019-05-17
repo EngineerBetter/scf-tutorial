f1af2711984ea4785c77fe0ad133cf4a83e765c5
Daniel Jones <daniel.jones@engineerbetter.com>
2019-05-17 08:42:12 +0100

    enable compute api too

--- ci/tf/gke/cluster.tf
+++ ci/tf/gke/cluster.tf
@@ -6,14 +6,19 @@ resource "random_id" "password" {
   byte_length = 16
 }
 
-resource "google_project_service" "container" {
-  service = "container.googleapis.com"
+resource "google_project_service" "compute" {
+  service                    = "compute.googleapis.com"
+  disable_dependent_services = true
+}
 
+resource "google_project_service" "container" {
+  service                    = "container.googleapis.com"
   disable_dependent_services = true
 }
 
 resource "google_container_cluster" "scf-cluster" {
   depends_on = [
+    "google_project_service.compute",
     "google_project_service.container",
   ]
 
