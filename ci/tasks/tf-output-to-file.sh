8e5841d00b4da874ff796256ce0aba8157179b5d
Daniel Jones <daniel.jones@engineerbetter.com>
2019-05-17 11:46:10 +0100

    add hardcoded path to output file

--- ci/tasks/tf-output-to-file.sh
+++ ci/tasks/tf-output-to-file.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 : "${METADATA_KEY:?METADATA_KEY env var must be provide the key name to look up in metadata}"
 : "${OUTPUT_FILE:?OUTPUT_FILE env var must provide full name of file to write into tf-output-to-file output dir}"
 
-jq -r ".${METADATA_KEY}" terraform/metadata > "${OUTPUT_FILE}"
+jq -r ".${METADATA_KEY}" terraform/metadata > tf-output-to-file/"${OUTPUT_FILE}"
