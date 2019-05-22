362923f1ae6d16e60a6082eed350e4325058404a
Daniel Jones <daniel.jones@engineerbetter.com>
2019-05-22 08:47:14 +0200

    create A record for UAA

--- ci/tf/gke/dns-zone.tf
+++ ci/tf/gke/dns-zone.tf
@@ -13,6 +13,16 @@ resource "google_dns_managed_zone" "scf" {
   ]
 }
 
+resource "google_dns_record_set" "frontend" {
+  name = "uaa.${google_dns_managed_zone.scf.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = "${google_dns_managed_zone.scf.name}"
+
+  rrdatas = ["${google_compute_address.uaa_ip.address}"]
+}
+
 output "scf_name_servers" {
   value = "${google_dns_managed_zone.scf.name_servers}"
 }
