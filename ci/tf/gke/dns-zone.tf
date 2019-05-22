da6baa91f110c362ea50d50278ecb8864a057228
Daniel Jones <daniel.jones@engineerbetter.com>
2019-05-22 11:11:42 +0200

    loadbalancers and dns for cf chart

--- ci/tf/gke/dns-zone.tf
+++ ci/tf/gke/dns-zone.tf
@@ -13,7 +13,11 @@ resource "google_dns_managed_zone" "scf" {
   ]
 }
 
-resource "google_dns_record_set" "frontend" {
+output "scf_name_servers" {
+  value = "${google_dns_managed_zone.scf.name_servers}"
+}
+
+resource "google_dns_record_set" "uaa" {
   name = "uaa.${google_dns_managed_zone.scf.dns_name}"
   type = "A"
   ttl  = 300
@@ -23,6 +27,32 @@ resource "google_dns_record_set" "frontend" {
   rrdatas = ["${google_compute_address.uaa_ip.address}"]
 }
 
-output "scf_name_servers" {
-  value = "${google_dns_managed_zone.scf.name_servers}"
+resource "google_dns_record_set" "scf_uaa_zone" {
+  name = "scf.uaa.${google_dns_managed_zone.scf.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = "${google_dns_managed_zone.scf.name}"
+
+  rrdatas = ["${google_compute_address.uaa_ip.address}"]
+}
+
+resource "google_dns_record_set" "ssh" {
+  name = "ssh.${google_dns_managed_zone.scf.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = "${google_dns_managed_zone.scf.name}"
+
+  rrdatas = ["${google_compute_address.ssh_ip.address}"]
+}
+
+resource "google_dns_record_set" "router" {
+  name = "*.${google_dns_managed_zone.scf.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = "${google_dns_managed_zone.scf.name}"
+
+  rrdatas = ["${google_compute_address.router_ip.address}"]
 }
--- ci/tf/gke/ips.tf
+++ ci/tf/gke/ips.tf
@@ -5,3 +5,23 @@ resource "google_compute_address" "uaa_ip" {
 output "uaa_ip" {
   value = "${google_compute_address.uaa_ip.address}"
 }
+
+resource "google_compute_address" "ssh_ip" {
+  name = "ssh"
+}
+
+output "ssh_ip" {
+  value = "${google_compute_address.ssh_ip.address}"
+}
+
+resource "google_compute_address" "router_ip" {
+  name = "router"
+}
+
+output "router_ip" {
+  value = "${google_compute_address.router_ip.address}"
+}
+
+output "uaa_ip" {
+  value = "${google_compute_address.uaa_ip.address}"
+}
