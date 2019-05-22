ae18ea0f7d4694164f4e6a40aa9f7c03ca29b9ce
Daniel Jones <daniel.jones@engineerbetter.com>
2019-05-22 16:26:58 +0200

    shorter ttl on GCP DNS records

--- ci/tf/gke/dns-zone.tf
+++ ci/tf/gke/dns-zone.tf
@@ -20,7 +20,7 @@ output "scf_name_servers" {
 resource "google_dns_record_set" "uaa" {
   name = "uaa.${google_dns_managed_zone.scf.dns_name}"
   type = "A"
-  ttl  = 300
+  ttl  = 5
 
   managed_zone = "${google_dns_managed_zone.scf.name}"
 
@@ -30,7 +30,7 @@ resource "google_dns_record_set" "uaa" {
 resource "google_dns_record_set" "scf_uaa_zone" {
   name = "scf.uaa.${google_dns_managed_zone.scf.dns_name}"
   type = "A"
-  ttl  = 300
+  ttl  = 5
 
   managed_zone = "${google_dns_managed_zone.scf.name}"
 
@@ -40,7 +40,7 @@ resource "google_dns_record_set" "scf_uaa_zone" {
 resource "google_dns_record_set" "ssh" {
   name = "ssh.${google_dns_managed_zone.scf.dns_name}"
   type = "A"
-  ttl  = 300
+  ttl  = 5
 
   managed_zone = "${google_dns_managed_zone.scf.name}"
 
@@ -50,7 +50,7 @@ resource "google_dns_record_set" "ssh" {
 resource "google_dns_record_set" "router" {
   name = "*.${google_dns_managed_zone.scf.dns_name}"
   type = "A"
-  ttl  = 300
+  ttl  = 5
 
   managed_zone = "${google_dns_managed_zone.scf.name}"
 
