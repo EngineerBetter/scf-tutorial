f45c7377bc7612265485a3f8630fdfbd7db70f31
Daniel Jones <daniel.jones@engineerbetter.com>
2019-05-22 06:53:39 +0200

    enable DNS API

--- ci/tf/gke/dns-zone.tf
+++ ci/tf/gke/dns-zone.tf
@@ -1,7 +1,16 @@
+resource "google_project_service" "dns" {
+  service                    = "dns.googleapis.com"
+  disable_dependent_services = true
+}
+
 resource "google_dns_managed_zone" "scf" {
   name        = "scf"
   dns_name    = "scf.engineerbetter.com."
   description = "DNS zone for SCF tutorial"
+
+  depends_on = [
+    "google_project_service.dns",
+  ]
 }
 
 output "scf_name_servers" {
